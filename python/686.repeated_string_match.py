6e1ba78ef816b767377392e97e16f0efa0261624
mttsai <mttsai@gmail.com>
2018-06-26 22:53:51 +0800

    686 update
        686.repeated_string_match.py

--- python/686.repeated_string_match.py
+++ python/686.repeated_string_match.py
@@ -1,32 +1,46 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
 class Solution(object):
-    def constructMaximumBinaryTree(self, nums):
+    def repeatedStringMatch(self, A, B):
         """
-        :type nums: List[int]
-        :rtype: TreeNode
+        :type A: str
+        :type B: str
+        :rtype: int
         """
-        if not nums:
-            return None
+        def kmp_f(s):
+            f = [0] * len(s) # Failure number
+            i, j = 1, 0
+            while i < len(s):
+                if j == 0 or s[i - 1] == s[j - 1]:
+                    f[i] = j
+                    i += 1
+                    j += 1
+                else:
+                    j = f[j]
+            return f
         
-        def max_tree(arrays):
-            if not arrays:
-                return None
-            
-            maxid, maxval = 0, arrays[0]
-            for idx, value in enumerate(arrays):
-                if value > maxval:
-                    maxid, maxval = idx, value
-                    
-            trie = TreeNode(maxval)
-            trie.left = max_tree(arrays[:maxid])
-            trie.right = max_tree(arrays[maxid + 1:])
+        def kmp(f, s1, s2):
+            if len(s1) > len(s2):
+                return False
 
-            return trie
+            i, j = 0, 0
+            while i < len(s1) and j < len(s2):
+                if s1[i] == s2[j]:
+                    i += 1
+                    j += 1
+                elif i == 0:
+                    j += 1
+                else:
+                    i = f[i]
+                        
+            return i == len(s1)
 
-        return max_tree(nums)
+        f = kmp_f(B)
+        reps = len(B) / len(A)
+        if kmp(f, B, A):
+            return 1
+        if kmp(f, B, A*reps):
+            return reps
+        if kmp(f, B, A*(reps + 1)):
+            return reps + 1
+        if kmp(f, B, A*(reps + 2)):
+            return reps + 2
+        return -1
